21300d0f4b93f82908fa2e196d1179b8b0078eba
ymataku <s256282@wakayama-u.ac.jp>
2022-05-22 20:29:59 +0900

    make it better

--- alart.js
+++
@@ -1,9 +0,0 @@
-function loadHello(){
-    alert("Hello!");
-  }
-function sum(){
-    let x = 3;
-    let y = 4;
-    let z = x + y;
-    alert(z);
-}
--- sample.rb
+++
@@ -1,6 +0,0 @@
-def sum
-    x = 3
-    y = 4
-    z = x + y
-    p z
-  end
